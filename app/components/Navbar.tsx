d4f2881b65ba376b8cf839aec0436b37659081a4
webify-io <webify.projects@gmail.com>
2024-12-31 11:33:17 +0200

    Completed Index Page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '@/public/logo.png';
+import ShinyButton from '@/components/ui/shiny-button';
+
+export function Navbar() {
+	return (
+		<div className="flex items-center justify-between py-5">
+			<Link href="/" className="flex items-center gap-2">
+				<Image src={Logo} alt="Logo" className="size-10 rounded-full" />
+				<h3 className="text-3xl font-semibold">
+					Invoice<span className="text-blue-600">Pro</span>
+				</h3>
+			</Link>
+
+			<Link href="/login">
+				<ShinyButton>Get Started</ShinyButton>
+			</Link>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import { Navbar } from './components/Navbar';
+import { Hero } from './components/Hero';
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Index Page</h1>
-		</div>
+		<main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+			<Navbar />
+			<Hero />
+		</main>
 	);
 }
