09acbace9c891ccd042b810650d79204db91b2f9
webify-io <webify.projects@gmail.com>
2025-01-02 15:38:48 +0200

    Update Navbar.tsx
    Mobile Responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,8 +7,12 @@ export function Navbar() {
 	return (
 		<div className="flex items-center justify-between py-5">
 			<Link href="/" className="flex items-center gap-2">
-				<Image src={Logo} alt="Logo" className="size-10 rounded-full" />
-				<h3 className="text-3xl font-semibold">
+				<Image
+					src={Logo}
+					alt="Logo"
+					className="size-8 md:size-10 rounded-full"
+				/>
+				<h3 className="text-2xl md:text-3xl font-semibold">
 					Invoice<span className="text-blue-600">Pro</span>
 				</h3>
 			</Link>
