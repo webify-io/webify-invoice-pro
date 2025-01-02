0b696d6a7173cd75bc7d176cc24663ca2698a66d
webify-io <webify.projects@gmail.com>
2025-01-02 16:07:08 +0200

    Update loading.tsx

--- app/dashboard/invoices/create/loading.tsx
+++ app/dashboard/invoices/create/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function LoadingFormCreation() {
 	return (
-		<div className="flex flex-col space-y-3 justify-center h-full w-full">
+		<div className="flex flex-col space-y-3 md:justify-center h-full w-full">
 			<Skeleton className="h-[125px] w-[250px] md:h-[75%] md:w-[65%] rounded-xl" />
 			<div className="space-y-2">
 				<Skeleton className="h-4 w-[250px] md:w-[30%]" />
