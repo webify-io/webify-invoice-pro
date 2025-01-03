5e28e7760f59bb8126c282ef1cc5305318641bce
webify-io <webify.projects@gmail.com>
2025-01-03 18:28:10 +0200

    Created Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export function Footer() {
+	return (
+		<footer className="flex flex-col md:flex-row mx-auto max-w-5xl px-5 items-center justify-center md:justify-between mt-16 mb-6 text-sm">
+			<p>InvoicePro &copy; {new Date().getFullYear()}.</p>
+			<p className="text-center">
+				Created by
+				<Link
+					href="https://www.webify.org.za/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<span className="font-semibold text-blue-600 hover:text-blue-400">
+						{' '}
+						Webify{' '}
+					</span>
+				</Link>
+				with Next.js, Mailtrap and Prisma
+			</p>
+		</footer>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
+import { Footer } from './components/Footer';
 
 export default function Home() {
 	return (
 		<main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 			<Navbar />
 			<Hero />
+			<Footer />
 		</main>
 	);
 }
